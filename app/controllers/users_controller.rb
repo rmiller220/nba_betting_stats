1043a8b76ce578965e6b30dadc8d7e5297f28dba
Reilly Miller <reillyjmiller31@gmail.com>
2023-08-30 15:10:47 -0600

    write create action for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id 
+      redirect_to user_path(@user)
+    else
+      redirect_to new_user_path
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password_digest)
+    end
 end
