9a2d90dba09b63b262bc0523a99eeb38c8d8c153
Reilly Miller <reillyjmiller31@gmail.com>
2023-08-30 15:38:56 -0600

    adjust tests for password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'validations' do
     before do
-      @user = User.create!(username: 'test', email: 'test@test.test', password_digest: 'password')
+      @user = User.create!(username: 'test', email: 'test@test.test', password: 'password')
     end
     it { should validate_presence_of(:username) }
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email)}
-    it { should validate_presence_of(:password_digest) }
+    it { should validate_presence_of(:password) }
   end
 end
