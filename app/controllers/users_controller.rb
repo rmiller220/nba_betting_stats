6b9d188be7ab68d8e8ea1277520fcb756d3ea026
Reilly Miller <reillyjmiller31@gmail.com>
2023-08-31 14:03:43 -0600

    update create, add login and login_form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,28 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(user_params)
-    if @user.save
-      session[:user_id] = @user.id 
-      redirect_to user_path(@user)
+    user = User.create(user_params)
+    if user.save
+      session[:user_id] = user.id 
+      redirect_to profile_path(user)
     else
       flash[:error] = "Invalid credentials. Please try again."
       redirect_to new_user_path
+    end
+  end
+
+  def login_form
+
+  end
+
+  def login
+      user = User.find_by(username: params[:username])
+    if user.authenticate(params[:password])
+      flash[:success] = "Welcome, #{user.username}!"
+      redirect_to root_path
+    else
+      flash[:error] = "Sorry, your credentials are bad."
+      render :login_form
     end
   end
 
