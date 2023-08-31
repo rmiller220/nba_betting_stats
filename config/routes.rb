73eda8fe266cff0731856390bfaf018fa96cc03b
Reilly Miller <reillyjmiller31@gmail.com>
2023-08-31 14:04:29 -0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:new, :create]
+  get '/profile', to: 'users#show'
   get '/', to: 'welcome#index'
+  get 'login', to: "users#login_form"
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
