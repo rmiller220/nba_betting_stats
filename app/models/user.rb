48fc13e444a825516ba66f22ebeb5d3d255d03ad
Reilly Miller <reillyjmiller31@gmail.com>
2023-09-01 13:19:00 -0600

    update model and test validations and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
-  validates_presence_of :password, require: true
+  validates_presence_of :password_digest, require: true
 
   has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,26 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'validations' do
     before do
-      @user = User.create!(username: 'test', email: 'test@test.test', password: 'password')
+      @user = User.create!(username: 'test', email: 'test@test.test', password: 'password', password_confirmation: 'password')
     end
     it { should validate_presence_of(:username) }
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email)}
     it { should validate_presence_of(:password) }
+    it { should have_secure_password }
+    
+    it "should validate password security" do
+      user = User.create(username: "test", email: "test@test.com", password: "password", password_confirmation: "password")
+      expect(user).to_not have_attribute(:password)
+      expect(user).to have_attribute(:password_digest)
+      expect(user.password_digest).to_not eq("password")
+    end
+    
+    it "should validate password confirmation" do
+      user = User.create(username: "test", email: "test@test.com", password: "password", password_confirmation: "notpassword")
+      expect(user).to_not have_attribute(:password)
+      expect(user.password_digest).to_not eq("password")
+      expect(user.save).to eq(false)
+    end
   end
 end
