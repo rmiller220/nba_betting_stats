b390186138a820ac58e46fc27d4035020d6b87b7
Reilly Miller <reillyjmiller31@gmail.com>
2023-09-01 13:20:49 -0600

    test sad path and update routes in tests

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe "New User page" do
         fill_in "Password confirmation", with: "password"
         click_button "Create User Profile"
         
-        expect(current_path).to eq(user_path(User.last))
+        expect(current_path).to eq(profile_path(User.last))
       end
     end
     
@@ -34,6 +34,61 @@ RSpec.describe "New User page" do
         fill_in "Email", with: "bob@bob.com"
         fill_in "Password", with: "password"
         fill_in "Password confirmation", with: "password"
+        click_button "Create User Profile"
+
+        expect(page).to have_content("Invalid credentials. Please try again.")
+        expect(current_path).to eq(new_user_path)
+      end
+
+      it "I can not create a new user without an email" do
+        fill_in "Username", with: "test"
+        fill_in "Password", with: "password"
+        fill_in "Password confirmation", with: "password"
+        click_button "Create User Profile"
+
+        expect(page).to have_content("Invalid credentials. Please try again.")
+        expect(current_path).to eq(new_user_path)
+      end
+
+      it "I can not create a new user without a unique username" do
+        user1 = User.create!(username: "test", email: "bob@bob.com", password: "password")
+        fill_in "Username", with: "test"
+        fill_in "Email", with: "tester1@tester.com"
+        fill_in "Password", with: "testing123"
+        fill_in "Password confirmation", with: "testing123"
+        click_button "Create User Profile"
+
+        expect(page).to have_content("Invalid credentials. Please try again.")
+        expect(current_path).to eq(new_user_path)
+      end
+
+      it "I can not create a new user without a unique email" do
+        user1 = User.create!(username: "test", email: "bob@bob.com", password: "password")
+        fill_in "Username", with: "bob"
+        fill_in "Email", with: "bob@bob.com"
+        fill_in "Password", with: "password"
+        fill_in "Password confirmation", with: "password"
+        click_button "Create User Profile"
+
+        expect(page).to have_content("Invalid credentials. Please try again.")
+        expect(current_path).to eq(new_user_path)
+      end
+
+      it 'I can not create a new user without a password' do
+        fill_in "Username", with: "test"
+        fill_in "Email", with: "bob@bob.com"
+        
+        click_button "Create User Profile"
+
+        expect(page).to have_content("Invalid credentials. Please try again.")
+        expect(current_path).to eq(new_user_path)
+      end
+
+      it "I can not create a new user without a matching password confirmation" do
+        fill_in "Username", with: "test"
+        fill_in "Email", with: "bob@bob.com"
+        fill_in "Password", with: "password"
+        fill_in "Password confirmation", with: "incorrect password"
         click_button "Create User Profile"
 
         expect(page).to have_content("Invalid credentials. Please try again.")
