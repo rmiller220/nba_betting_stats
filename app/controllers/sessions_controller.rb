80e5605e00d0cd45d7f7196eccde66066440aa6c
Reilly Miller <reillyjmiller31@gmail.com>
2023-09-01 13:19:14 -0600

    comment out session controller actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
-  def new
+  # def new
 
-  end
+  # end
 
-  def create
-    user = User.find_by(email: params[:email])
-    session[:user_id] = user.id
-    flash[:success] = "Welcome, #{user.username}!"
-    redirect_to '/profile'
-  end
+  # def create
+  #   user = User.find_by(email: params[:email])
+  #   session[:user_id] = user.id
+  #   flash[:success] = "Welcome, #{user.username}!"
+  #   redirect_to '/profile'
+  # end
 end
