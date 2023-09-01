aa1c63a9af4bec1fe0677964b16a0ceb6e83c4f3
Reilly Miller <reillyjmiller31@gmail.com>
2023-09-01 13:20:33 -0600

    create new routes and remove unused ones

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
+
   resources :users, only: [:new, :create]
   get '/profile', to: 'users#show'
-  get '/', to: 'welcome#index'
+  root to: 'welcome#index'
   get 'login', to: "users#login_form"
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post '/login', to: 'users#login_user'
+  delete '/logout', to: "users#logout"
+  
 end
