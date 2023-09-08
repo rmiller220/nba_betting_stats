2591d14e436c86cd9e9944de18b6043b6c5d1d6b
Reilly Miller <reillyjmiller31@gmail.com>
2023-09-08 11:21:21 -0600

    add test for edit link, reorganize tests

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "User show page" do
+  before do 
+    @user1 = User.create!(username: "test", email: "bob@bob.com", password: "password", password_confirmation: "password")
+    @user2 = User.create!(username: "new_test", email: "tom@tom.com", password: "12345", password_confirmation: "12345")
+  end
   describe "As a visitor" do
-    before do 
-      @user1 = User.create!(username: "test", email: "bob@bob.com", password: "password", password_confirmation: "password")
-      @user2 = User.create!(username: "new_test", email: "tom@tom.com", password: "12345", password_confirmation: "12345")
-    end
     it "I can see a user's profile page" do
       visit root_path
       click_link "Login with username or email"
@@ -17,11 +17,25 @@ RSpec.describe "User show page" do
       expect(page).to have_content("Welcome to your dashboard, #{@user1.username}!")
       expect(page).to have_content("Your email is #{@user1.email}")
       expect(page).to have_content("Need to change your profile?")
+      expect(page).to have_link("Edit Profile")
       visit root_path
       click_button "Log Out"
     end
 
-    it "sad path: I can't see a user's profile page if I'm not logged in" do
+    it "Edit profile link takes me to edit profile page" do
+      visit root_path
+      click_link "Login with username or email"
+      fill_in :username, with: @user1.username
+      fill_in :password, with: @user1.password
+      click_button "Login"
+      visit profile_path(@user1)
+      click_link "Edit Profile"
+
+      expect(current_path).to eq(edit_profile_path(@user1))
+    end
+  end
+  describe "sad path" do
+    it "I can't see a user's profile page if I'm not logged in" do
       visit profile_path(@user1)
 
       expect(page).to have_content("Only the user can view their profile.")
